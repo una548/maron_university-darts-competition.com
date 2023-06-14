2b12dbe51f8f3bfa20e22efaff28f4db1a2fc614
una548 <una021114@gmail.com>
2023-06-14 15:38:55 +0900

    PC All

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+$(function($){
+    $('.first-view-switcher').bgSwitcher({
+        images:['image/firstview1.jpg','image/firstview2.jpg','image/firstview3.jpg'],
+        interval: 3000,
+        loop: true,
+    });
+});
