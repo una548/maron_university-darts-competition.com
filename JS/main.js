0223520aa74d79897bc70ba6ce299afdf09bd1b7
una548 <una021114@gmail.com>
2023-06-15 22:28:37 +0900

    responsive

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,21 @@
 $(function($){
+var sp='_sp.';
+var pc='_pc.';
+
+
     $('.first-view-switcher').bgSwitcher({
-        images:['image/firstview1.jpg','image/firstview2.jpg','image/firstview3.jpg'],
+        images:['image/firstview1.jpg','image/firstview2.jpg', 'image/firstview3.jpg'],
         interval: 3000,
         loop: true,
     });
+    $(".openbtn").click(function () {//ボタンがクリックされたら
+        $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+        $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    });
+    
+    $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+        $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+    });
 });
+
